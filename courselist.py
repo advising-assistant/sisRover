e4079a000b842221394981c6e112b782c4265877
Sean Gatewood <stg2bd@virginia.edu>
2020-01-21 18:27:18 -0500

    Add basic caching

--- courselist.py
+++ courselist.py
@@ -5,6 +5,7 @@ import requests
 import json
 from course import Course
 import urllib.parse
+import os
 
 def dict_to_url_params(d):
     return "?" + "&".join([str(key) + "=" + urllib.parse.quote(str(value),safe="") for key,value in d.items()])
@@ -36,6 +37,7 @@ url_params = {
 } # page left out
 
 BASE_URL = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch"
+CACHE_PATH = "cache.json"
 
 def makeRequest(search_mnem, term, page):
     global url_params
@@ -46,29 +48,47 @@ def makeRequest(search_mnem, term, page):
     results_url = s.get(url)
     return results_url.text
 
+# ["[1,2]","[3,4]"] -> "[1,2,3,4]"
+def combine_list_strings(list_strings):
+    remove_brackets = lambda i: i[1:-1]
+    return "[%s]" % (",".join(map(remove_brackets,list_strings)))
+
+def refresh_cache(search_mnem,term):
+    page = 1
+    results = [] # list of strings representing lists
+
+    nextresults = makeRequest(search_mnem, term, page)
+
+    while nextresults != "[]":
+        results.append(nextresults)
+        print("Page", page, "loaded...\r", end="")
+
+        page += 1
+        nextresults = makeRequest(search_mnem, term, page)
+
+    results = combine_list_strings(results)
+
+    print("Found",page-1,"pages of results.")
+
+    with open(CACHE_PATH,"w") as y:
+        y.write(results)
+
 
 class CourseList:
     def __init__(self, search_mnem, term):
         self.mnemonic = search_mnem
         self.term = term
-        self.courses = self._doSearch(search_mnem, term)
+        self.courses = self._doSearch(search_mnem, term, use_cache=True)
         print("Done!")
 
-
-    def _doSearch(self, search_mnem, term):
-        page = 1
-        nextresults = makeRequest(search_mnem, term, page)
+    def _doSearch(self, search_mnem, term, use_cache=True):
 
         jresults = []
 
-        while nextresults != '[]':
-            print("Page", page, "parsed...\r", end="")
-            page += 1
+        if not use_cache or not os.path.exists(CACHE_PATH):
+            refresh_cache(search_mnem,term)
 
-            jresults += json.loads(nextresults)
-
-            nextresults = makeRequest(search_mnem,term,page)
-
-        print("Found",page-1,"pages of results.")
+        with open(CACHE_PATH) as y:
+            jresults = json.load(y)
 
         return [Course(c) for c in jresults]
